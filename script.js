1392c3d1df1cc24f0225f80bc695c704c8a11497
sabagoglichidze <saba.goglichidze.educate@gmail.com>
2026-08-17 13:53:26 +0400

    Add JavaScript variables assignment

---
+++ script.js
@@ -0,0 +1,11 @@
+// 1. შევქმნათ 4 ცვლადი
+const firstName = "საბა";
+const lastName = "გოგლიჩიძე";
+const email = "sabagoglika@gmail.com";
+const age = 25;
+
+// 2. ოთხი console.log() ცვლადების გამოყენებით
+console.log(`მე მქვია ${firstName}`);
+console.log(`ჩემი გვარი არის ${lastName}`);
+console.log(`ჩემი იმეილი არის ${email}`);
+console.log(`ჩემი ასაკია: ${age}`);
